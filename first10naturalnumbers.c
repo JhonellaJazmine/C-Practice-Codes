cef51993ef03874d5d154d6a025319a106cfd1ef
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2020-12-14 20:34:08 +0800

    Create first10naturalnumbers.c

---
+++ first10naturalnumbers.c
@@ -0,0 +1,10 @@
+//Write a program in C to display the first 10 natural numbers.
+#include <stdio.h>
+
+int main()
+{
+    for(int i=0;i<10;i++)   
+    {
+      printf("%d",i+1);       // displays i+1 number
+    }
+}
