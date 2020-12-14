b9fc078923c45c0e66bc1b3d631e58e0f4b0ca0f
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2020-12-14 22:53:58 +0800

    Create righttriangle.c

---
+++ righttriangle.c
@@ -0,0 +1,16 @@
+// Write a program in C to display the pattern like right angle triangle using an asterisk.
+#include <stdio.h>
+
+int main()
+{
+    //declaration of variables
+    int n,sum=0;
+
+    for(int i=0;i<4;i++){             //loop for printing right angle triangle
+        for(int j=0;j<i+1;j++)        //loop for printing asterisk in a line
+        {
+            printf("*");            
+        }
+        printf("\n");          //prints new line      
+    }
+}
